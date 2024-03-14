dd5ee6ca4d24cf96d346bae256f415555f35e622
Akshay4452 <akky123654@gmail.com>
2024-03-14 20:12:53 +0530

    Added Generic Event Controller
    Game is Buggy

--- Assets/Scripts/Interactables/KeyView.cs
+++ Assets/Scripts/Interactables/KeyView.cs
@@ -2,13 +2,18 @@ using UnityEngine;
 
 public class KeyView : MonoBehaviour, IInteractable
 {
-    [SerializeField] GameUIView gameUIView;
+    //private void OnEnable() => EventService.Instance.OnKeyPickedUp.AddListener(onKeyPickUp);
+    //private void OnDisable() => EventService.Instance.OnKeyPickedUp.RemoveListener(onKeyPickUp);
     public void Interact()
     {
+        int currentKeys = GameService.Instance.GetPlayerController().KeysEquipped;
+
         GameService.Instance.GetInstructionView().HideInstruction();
         GameService.Instance.GetSoundView().PlaySoundEffects(SoundType.KeyPickUp);
-        GameService.Instance.GetPlayerController().KeysEquipped++;
-        gameUIView.UpdateKeyText();
+
+        currentKeys++;
+        // Invoke the key pick up event after incrementing the keys
+        GameService.Instance.GetGameUI().UpdateKeyText();
 
         gameObject.SetActive(false);
     }
